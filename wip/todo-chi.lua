dc616ecc5792b09aa24f64b4a354a6b8fd7ea5ef
Chibloom <m3gamanbg@gmail.com>
2026-02-24 22:17:02 -0800

    odyssey sheol
    first part loaded successfully in odyB
    
    data has only subzone, wip shouldn't be read by mobdb
    
    still some stuff to wip on: UNM resists, cruel joke

---
+++ wip/todo-chi.lua
@@ -0,0 +1,19 @@
+https://github.com/Windower/Resources/blob/master/resources_data/zones.lua
+https://github.com/Windower/Resources/blob/master/resources_data/jobs.lua
+https://github.com/MarianArlt/sheolhelper/blob/main/resistances.lua
+https://github.com/MarianArlt/sheolhelper/blob/main/types.lua
+https://www.ffxiah.com/forum/topic/58215/list-of-mobs-and-damage-types-in-sheol-c/#3731873
+https://www.ffxiah.com/forum/topic/54555/the-odyssey-strategy-and-discussion/152/#3590215
+xitinker
+
+        ['Alexander'] = { Name='Alexander', Notorious=xxx, Aggro=true, Link=true, TrueSight=true, Job=0, MinLevel=xxx, MaxLevel=xxx, Immunities=0, Respawn=0, Sight=xxx, Sound=xxx, Blood=false, Magic=false, JA=false, Scent=false, Drops={}, Spells={}, Modifiers={Slashing=1, Piercing=1, H2H=1, Impact=1, Fire=1, Ice=1, Wind=1, Earth=1, Lightning=1, Water=1, Light=1, Dark=1} },
+
+    [294] = {id=294,en="Dynamis - San d'Oria [D]",ja="デュナミス-サンドリア〔Ｄ〕",can_pet=true,is_layered=true,search="DynamisSan_D"},
+    [295] = {id=295,en="Dynamis - Bastok [D]",ja="デュナミス-バストゥーク〔Ｄ〕",can_pet=true,is_layered=true,search="DynamisBas_D"},
+    [296] = {id=296,en="Dynamis - Windurst [D]",ja="デュナミス-ウィンダス〔Ｄ〕",can_pet=true,is_layered=true,search="DynamisWin_D"},
+    [297] = {id=297,en="Dynamis - Jeuno [D]",ja="デュナミス-ジュノ〔Ｄ〕",can_pet=true,is_layered=true,search="DynamisJeu_D"},
+
+    [275] = {id=275,en="Outer Ra'Kaznar [U1]",ja="ラ・カザナル宮外郭〔Ｕ１〕",can_pet=true,is_layered=true,search="OutRaKaz_U1"},
+    [133] = {id=133,en="Outer Ra'Kaznar [U2]",ja="ラ・カザナル宮外郭〔Ｕ２〕",can_pet=true,is_layered=true,search="OutRaKaz_U2"},
+    [189] = {id=189,en="Outer Ra'Kaznar [U3]",ja="ラ・カザナル宮外郭〔Ｕ３〕",can_pet=true,is_layered=true,search="OutRaKaz_U3"},
+        --sortie and vagery
